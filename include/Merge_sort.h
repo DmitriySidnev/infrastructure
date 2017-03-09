f9323ec33023793e63b88c3bf07c80502ce23fc4
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-09 19:49:53 +0300

    Merge_sort

---
+++ include/Merge_sort.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_MERGE_SORT_H_
+#define INCLUDE_MERGE_SORT_H_
+#include <vector>
+
+std::vector<int> merge(std::vector<int> v, int l, int m, int r);
+
+#endif  // INCLUDE_MERGE_SORT_H_
