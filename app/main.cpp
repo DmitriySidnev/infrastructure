c2678411c68911965a36cba7122448a2965e6527
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-10 17:54:35 +0300

    OK

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,6 @@
 #include "add_mul.h"
-#include "merge_sort.h"
 #include <iostream>
 
 int main() {
   std::cout << "2 + 2 = " << add(2, 2) << std::endl;
-  std::cout << "Merge sort complete" << std::endl;
-  std::cout << "Merge sort complete2" << std::endl;
 }
