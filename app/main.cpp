4d9b004f215c9bb8064f79e6e48689a9a0569cbc
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-12 16:15:37 +0300

    some travis errors

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,5 @@
-#include "add_mul.h"
-#include "Merge_sort.h"
 #include <iostream>
 
 int main() {
-  std::cout << "2 + 2 = " << add(2, 2) << std::endl;
+  std::cout << "Hash complete" << std::endl;
 }
