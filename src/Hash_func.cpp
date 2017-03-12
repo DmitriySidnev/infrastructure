a1ec7b4516a1f5d9f966ab663d280b4847825cbc
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-12 18:21:44 +0300

    OK

--- src/Hash_func.cpp
+++ src/Hash_func.cpp
@@ -2,78 +2,39 @@
 #include <vector>
 #include <list>
 
-class MyHash::hash{
- protected:
-  int key;
-  int size;
-  const int step = 37;
-
-  explicit hash(int a = 1000) {
-    size = a;
+int MyHash::hash::H(int key) {
+  int key_first = key;
+  if (key > size - 1) {
+    key %= size;
   }
+  return key;
+}
 
-  int H(int key) {
-    int key_first = key;
-    if (key > size - 1) {
-      key %= size;
-    }
-
-    return key;
-  }
-};
-
-class MyHash::hash_mix : hash {
-  std::vector<std::pair<int, int>> buf;
-
- public:
-  explicit hash_mix(int a = 1000) : buf(size) {}
-};
-
-class MyHash::hash_chain : hash {
-  std::vector<std::list<std::pair<int, int>>> buf;
-
- private:
-  std::list<std::pair<int, int>>::iterator find(int key) {
-    std::list<std::pair<int, int>>::iterator it = buf[key].begin();
-
-    while (it != buf[key].end()) {
-      if ((*it).first == key) {
-        return it;
-      }
-      ++it;
-    }
-
-    return it;
-  }
-
- public:
-  explicit hash_chain(int a = 1000) : buf(size) {}
-
-  void insert(int key, int value) {  // insert value with key
-    key = H(key);
-    if (find(key) == buf[key].end()) {
-      buf[key].list::push_back(std::make_pair(key, value));
-    } else {
-      std::list<std::pair<int, int>>::iterator it = find(key);
-      (*it).second = value;
-    }
-  }
-
-  void remove(int key) {  // delete entry with key
-    key = H(key);
+void MyHash::hash_chain::insert(int key, int value) {  // insert value with key
+  key = H(key);
+  if (find(key) == buf[key].end()) {
+    buf[key].list::push_back(std::make_pair(key, value));
+  } else {
     std::list<std::pair<int, int>>::iterator it = find(key);
-    if (it != buf[key].end()) {
-      buf[key].list::erase(it);
-    }
+    (*it).second = value;
   }
+}
 
-  int get(int key) {  // get entry with key
-    key = H(key);
-    std::list<std::pair<int, int>>::iterator it = find(key);
-    if (it != buf[key].end()) {
-      return (*it).second;
-    } else {
-      return 0;
-    }
+void MyHash::hash_chain::remove(int key) {  // delete entry with key
+  key = H(key);
+  std::list<std::pair<int, int>>::iterator it = find(key);
+  if (it != buf[key].end()) {
+    buf[key].list::erase(it);
   }
-};
+}
+
+int MyHash::hash_chain::get(int key) {  // get entry with key
+  key = H(key);
+  std::list<std::pair<int, int>>::iterator it = find(key);
+  if (it != buf[key].end()) {
+    return (*it).second;
+  } else {
+    return 0;
+  }
+}
+
