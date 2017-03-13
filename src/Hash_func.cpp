c4e81c76502683880dcd1b7b52117188fcd893ec
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-13 20:37:13 +0300

    fixed travis error

--- src/Hash_func.cpp
+++ src/Hash_func.cpp
@@ -1,37 +1,38 @@
 #include "hash_func.h"
 #include <vector>
 #include <list>
+#include <cstddef>
 
 namespace MyHash {
 
-  size_t hash::hash_func(size_t key) {
+  std::size_t hash::hash_func(std::size_t key) {
   if (key > size - 1) {
     key %= size;
   }
   return key;
 }
 
-void hash_chain::insert(size_t key, size_t value) {  // insert value with key
-  size_t key_new = hash_func(key);
+void hash_chain::insert(std::size_t key, std::size_t value) {  // insert value with key
+  std::size_t key_new = hash_func(key);
   if (find(key_new) == buf[key_new].end()) {
     buf[key_new].list::push_back(std::make_pair(key_new, value));
   } else {
-    std::list<std::pair<size_t, size_t>>::iterator it = find(key_new);
+    std::list<std::pair<std::size_t, std::size_t>>::iterator it = find(key_new);
     it->second = value;
   }
 }
 
-void hash_chain::remove(size_t key) {  // delete entry with key
-  size_t key_new = hash_func(key);
-  std::list<std::pair<size_t, size_t>>::iterator it = find(key_new);
+void hash_chain::remove(std::size_t key) {  // delete entry with key
+  std::size_t key_new = hash_func(key);
+  std::list<std::pair<std::size_t, std::size_t>>::iterator it = find(key_new);
   if (it != buf[key_new].end()) {
     buf[key_new].list::erase(it);
   }
 }
 
-size_t hash_chain::get(size_t key) {  // get entry with key
-  size_t key_new = hash_func(key);
-  std::list<std::pair<size_t, size_t>>::iterator it = find(key_new);
+std::size_t hash_chain::get(std::size_t key) {  // get entry with key
+  std::size_t key_new = hash_func(key);
+  std::list<std::pair<std::size_t, std::size_t>>::iterator it = find(key_new);
   if (it != buf[key_new].end()) {
     return it->second;
   } else {
