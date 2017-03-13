d648169e3e84eb1ed596a9e15cefe41114d7c24b
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-13 19:41:10 +0300

    fix bugs

--- include/hash_func.h
+++ include/hash_func.h
@@ -4,46 +4,46 @@
 #include <list>
 
 namespace MyHash {
-class hash {
- public:
-  int key;
-  int size = 1000;
-  const int step = 37;
+  class hash {
+  public:
+    //int key;
+    int size;
+    const int step = 37;
 
- protected:
-  hash() {}
-  int hash_func(int key);
-};
+  protected:
+    hash(int new_size = 1000) : size(new_size) {}
+    int hash_func(int key);
+  };
 
-class hash_mix : hash {
-  std::vector<std::pair<int, int>> buf;
+  class hash_mix : hash {
+    std::vector<std::pair<int, int>> buf;
 
- public:
-  explicit hash_mix(int a = 1000) : buf(a) {}
-};
+  public:
+    explicit hash_mix(int a = 1000) : buf(a) {}
+  };
 
-class hash_chain : hash {
-  std::vector<std::list<std::pair<int, int>>> buf;
+  class hash_chain : hash {
+    std::vector<std::list<std::pair<int, int>>> buf;
 
- private:
-  std::list<std::pair<int, int>>::iterator find(int key) {
-    std::list<std::pair<int, int>>::iterator it = buf[key].begin();
+  private:
+    std::list<std::pair<int, int>>::iterator find(int key) {
+      std::list<std::pair<int, int>>::iterator it = buf[key].begin();
 
-    while (it != buf[key].end()) {
-      if (it->first == key) {
-        return it;
+      while (it != buf[key].end()) {
+        if (it->first == key) {
+          return it;
+        }
+        ++it;
       }
-      ++it;
+      return it;
     }
-    return it;
-  }
 
- public:
-  explicit hash_chain(int a = 1000) : buf(a) {}
-  void insert(int key, int value);
-  void remove(int key);
-  int get(int key);
-};
+  public:
+    explicit hash_chain(int new_size = 1000) : hash(new_size), buf(new_size) {}
+    void insert(int key, int value);
+    void remove(int key);
+    int get(int key);
+  };
 }  // namespace MyHash
 
 #endif  // INCLUDE_HASH_FUNC_H_
