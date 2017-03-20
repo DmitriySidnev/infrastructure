0938f1292f01bed5ce2c80b16415df9f280ab666
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-20 19:21:00 +0300

    Fix travis's space-errors

--- src/MyInt.cpp
+++ src/MyInt.cpp
@@ -7,31 +7,31 @@ class Integer{
 
  public:
   explicit Integer(int v0 = 0) : val(v0) {}
-  explicit Integer (double d) {
-    val = int(d);
-	}
+  explicit Integer (double d){
+    val = static_cast<int>(d);
+  }
 
-  operator int(){
+  operator int() {
     return val;
-	}
+  }
 
   //  basic arithmetic operators	
-  const Integer operator+(const Integer& r){
+  const Integer operator+(const Integer& r) {
     Integer res;
     res.val = val + r.val;
     return res;
   }
-  const Integer operator-(const Integer& r){
+  const Integer operator-(const Integer& r) {
     Integer res;
     res.val = val - r.val;
     return res;
   }
-  const Integer operator*(const Integer& r){
+  const Integer operator*(const Integer& r) {
     Integer res;
     res.val = val * r.val;
     return res;
   }
-  const Integer operator/(const Integer& r){
+  const Integer operator/(const Integer& r) {
     Integer res;
     res.val = val / r.val;
     return res;
@@ -52,56 +52,56 @@ class Integer{
     val++;
     return *this;
   }
-  friend Integer operator++(Integer& l, int){
+  friend Integer operator++(Integer& l, int) {
     Integer res = l;
     l.val += 1;
     return res;
   }
 
   //  operator ==, !=
-  friend bool operator==(const Integer& l, const Integer& r){
+  friend bool operator==(const Integer& l, const Integer& r) {
     return l.val == r.val;
   }
-  friend bool operator!=(const Integer& l, const Integer& r){
+  friend bool operator!=(const Integer& l, const Integer& r) {
     return l.val != r.val;
   }
 
   //  opeator+
   /*
-  friend Integer& operator+(const Integer& l, const Integer& r){
+  friend Integer& operator+(const Integer& l, const Integer& r) {
     Integer res = l;
     res.val += r.val;
     return res;
   }
   */
-  friend Integer operator+(int l, const Integer& r){
+  friend Integer operator+(int l, const Integer& r) {
     Integer res;
     res.val = l + r.val;
     return res;
   }
-  friend Integer operator+(const Integer& l, int r){
+  friend Integer operator+(const Integer& l, int r) {
     Integer res;
     res.val = l.val + r;
     return res;
   }
   friend Integer operator+(double l, const Integer& r) {
     Integer res;
-    res.val = int(l) + r.val;
+    res.val = static_cast<int>(l) + r.val;
     return res;
   }
   friend Integer operator+(const Integer& l, double r) {
     Integer res;
-    res.val = l.val + int(r);
+    res.val = l.val + static_cast<int>(r);
     return res;
   }
 
   //  operator[]
-  Integer& operator[](const Integer& a){
+  Integer& operator[](const Integer& a) {
     Integer res = a;
     return res;
   }
   //  operator=
-  Integer& operator=(const Integer& r){
+  Integer& operator=(const Integer& r) {
     val = r.val;
     return *this;	
   }
@@ -110,28 +110,27 @@ class Integer{
     return *this;
   }
   //  cin >>, cout <<
-  friend std::ostream& operator<<(std::ostream& outStream, const Integer& r){
+  friend std::ostream& operator<<(std::ostream& outStream, const Integer& r) {
     outStream << r.val;
     return outStream;
   }
-  friend std::istream& operator>>(std::istream& inStream, Integer& r){
+  friend std::istream& operator>>(std::istream& inStream, Integer& r) {
     inStream >> r.val;
     return inStream;
   }
   //  get()
-  int get() const{
+  int get() const {
     return val;
   }
 };	
-}
+} //  namespace MyInt
 
 int main(){
-
   MyInt::Integer x(5), y(3.5), z, xf[10];
   z = x + y;
   int f = z.get();
 
-  if (x != y){
+  if (x != y) {
     std::cout << "x != y" << std::endl;
   } else {
     std::cout << "x == y" << std::endl;
