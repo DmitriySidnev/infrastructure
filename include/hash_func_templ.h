635db78598507ab562e051a940d9081ab4ac2242
Dmitriy Sidnev <sidnew1987@gmail.com>
2017-03-27 18:03:59 +0300

    Hash template: travis' errors fixed

--- include/hash_func_templ.h
+++ include/hash_func_templ.h
@@ -27,7 +27,7 @@ template<class T1, class T2> class hash_mix : Hash<T1> {
   explicit hash_mix(std::size_t a = 1000) : buf(a) {}
 };
 
-template<class T1, class T2> class hash_chain : Hash<T1> {
+template<class T1, class T2> class hash_chain : public Hash<T1> {
   std::vector<std::list<std::pair<T1, T2>>> buf;
 
  private:
@@ -47,7 +47,7 @@ template<class T1, class T2> class hash_chain : Hash<T1> {
   explicit hash_chain(size_t new_s = 1000) : Hash<T1>(new_s), buf(new_s) {}
 
   void insert(const T1 key, const T2 value) {
-    std::size_t key_new = hash_func(&key);
+    std::size_t key_new = Hash<T1>::hash_func(&key);
     typename std::list<std::pair<T1, T2>>::iterator it = find_value(key_new, key);
 
     if (it == buf[key_new].end()) {
@@ -58,7 +58,7 @@ template<class T1, class T2> class hash_chain : Hash<T1> {
   }
 
   void remove(T1 key) {
-    std::size_t key_new = hash_func(&key);
+    std::size_t key_new = Hash<T1>::hash_func(&key);
     typename std::list<std::pair<T1, T2>>::iterator it = find_value(key_new, key);
     if (it != buf[key_new].end()) {
       buf[key_new].erase(it);
@@ -67,7 +67,7 @@ template<class T1, class T2> class hash_chain : Hash<T1> {
 
   T2* find(const T1 key) {
     T2* out_val = nullptr;
-    std::size_t key_new = hash_func(&key);
+    std::size_t key_new = Hash<T1>::hash_func(&key);
     typename std::list<std::pair<T1, T2>>::iterator it = find_value(key_new, key);
     if (it != buf[key_new].end()) {
       out_val = &it->second;
